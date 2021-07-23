43c3a85167ae28c0f540b1bbe068fab5f8d0ddd1
Maksim <semimaks@mail.ru>
2021-07-23 12:32:17 +0300

    Программа сохраняет в словарь имена и даты дня рождения.

---
+++ birthdays.py
@@ -0,0 +1,33 @@
+# Эта программа применяет словарь для хранения
+# имён и дней рождения друзей.
+
+# Глобальные константы для пунктов меню.
+LOOK_UP = 1
+ADD = 2
+CHANGE = 3
+DELETE = 4
+QUIT = 5
+
+# Главная функция.
+def main():
+    # Создать пустой словарь
+    birthdays = {}
+
+    # Инициализировать переменную для выбора пользователя.
+    choice = 0
+
+    while choice != QUIT:
+        # Получить выбранный пользователем пункт меню.
+        choice = get_menu_choice()
+
+    # Обработать выбранный вариант действий.
+    if choice == LOOK_UP:
+        look_up(birthdays)
+    elif choice == ADD:
+        add(birthdays)
+    elif choice == CHANGE:
+        change(birthdays)
+    elif choice == DELETE:
+        delete(birthdays)
+    else:
+        choice = input('Введите допустимое значение меню.')
